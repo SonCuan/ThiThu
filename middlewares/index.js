31c563f976bc6c0c47796773a17dc1fb6910a145
SonCuan <sonquan2611@gmail.com>
2024-08-12 22:52:30 +0700

    checkPermision

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,1 +1,39 @@
-// code base
+import { decode } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const checkPermission = async (req, res , next  ) => { 
+    try {
+        if(!req.headers.authorization){
+            return res.status(401).json({
+                message : "Ban chua dang nhap"
+            })
+        }
+        const token = req.headers.authorization.split(" ")[1];
+        if(!token){
+            return res.status(401).json({
+                message:  "Ban chua dang nhap",
+            })
+        }
+        const decode = jwt.verify(token, "quanns");
+        if(!decode){
+            return res.status(401).json({
+                message : "Token ko hop le",
+            })
+        }
+        const user = await User.findById(decode._id);
+        if(!user) {
+            return res.status(401).json({
+                message : "Nguoi dung ko ton tai",
+            })
+        }
+        if(user.role !== "admin"){
+            return res.status(403).json({
+                message : "Ban khong co quyen truy cap vao",
+            })
+        }
+        next();
+    } catch (error) {
+        return res.status(500).json({message : error.message})
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@
 import  express  from 'express';
 import { createProduct, getAll, getDetail, getUpdate, remove } from '../controllers/product.js';
 import { login, signup } from './../controllers/auth.js';
+import { checkPermission } from '../middlewares/index.js';
 
 const router = express.Router();
-router.post("/product", createProduct);
+router.post("/product", checkPermission, createProduct);
 router.get("/product/:id", getDetail);
 router.get("/product", getAll);
-router.patch("/product/:id", getUpdate);
-router.delete("/product/:id", remove);
+router.patch("/product/:id", checkPermission, getUpdate);
+router.delete("/product/:id",checkPermission, remove);
 // user
 router.post("/login", login);
 router.post("/register" , signup);
